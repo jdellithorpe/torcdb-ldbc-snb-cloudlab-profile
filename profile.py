8d83d749ae6aa5cda75b3214213f8db63e00c304
Jonathan Ellithorpe <jde@cs.stanford.edu>
2018-01-20 02:32:33 +0000

    Turned vlan tagging off on rclan.

--- profile.py
+++ profile.py
@@ -101,7 +101,7 @@ request = pc.makeRequestRSpec()
 # Create a local area network for the RAMCloud cluster.
 rclan = request.LAN("rclan")
 rclan.best_effort = True
-rclan.vlan_tagging = True
+rclan.vlan_tagging = False
 rclan.link_multiplexing = True
 
 # Create a special network for connecting datasets to rcnfs.
