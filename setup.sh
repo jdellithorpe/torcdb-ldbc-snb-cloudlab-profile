0a523b7911991db02d998bc8cfee39447a19bca0
Jonathan Ellithorpe <jde@cs.stanford.edu>
2018-01-22 22:47:54 +0000

    Turned over /etc/hosts config.

--- setup.sh
+++ setup.sh
@@ -239,23 +239,23 @@ then
 fi
 
 # Add machines on control network to /etc/hosts
-echo -e "\n===== ADDING CONTROL NETWORK HOSTS TO /ETC/HOSTS ====="
-hostArray=("rcmaster" "rcnfs")
-for i in $(seq 1 $NUM_RCNODES)
-do
-  host=$(printf "rc%02d" $i)
-  hostArray=("${hostArray[@]}" "$host")
-done
-
-for host in ${hostArray[@]}
-do
-  while ! $(ssh $host "hostname -i")
-  do
-    sleep 1
-    echo "Waiting for $host to come up..."
-  done
-  echo $(ssh $host "hostname -i")" "$host-ctrl >> /etc/hosts
-done
+#echo -e "\n===== ADDING CONTROL NETWORK HOSTS TO /ETC/HOSTS ====="
+#hostArray=("rcmaster" "rcnfs")
+#for i in $(seq 1 $NUM_RCNODES)
+#do
+#  host=$(printf "rc%02d" $i)
+#  hostArray=("${hostArray[@]}" "$host")
+#done
+#
+#for host in ${hostArray[@]}
+#do
+#  while ! $(ssh $host "hostname -i")
+#  do
+#    sleep 1
+#    echo "Waiting for $host to come up..."
+#  done
+#  echo $(ssh $host "hostname -i")" "$host-ctrl >> /etc/hosts
+#done
 
 # RCMaster specific configuration.
 if [ $(hostname --short) == "rcmaster" ]
