dd5d8056523945816f3635873e034a4b802a4ca0
Jonathan Ellithorpe <jde@cs.stanford.edu>
2018-06-19 22:19:30 +0000

    Added gnuplot to list of installed software.

--- system-setup.sh
+++ system-setup.sh
@@ -83,7 +83,7 @@ fi
 # Common utilities
 echo -e "\n===== INSTALLING COMMON UTILITIES ====="
 apt-get update
-apt-get --assume-yes install mosh vim tmux pdsh tree axel htop ctags cscope cmake clang
+apt-get --assume-yes install mosh vim tmux pdsh tree axel htop ctags cscope cmake clang gnuplot
 # NFS
 echo -e "\n===== INSTALLING NFS PACKAGES ====="
 apt-get --assume-yes install nfs-kernel-server nfs-common
