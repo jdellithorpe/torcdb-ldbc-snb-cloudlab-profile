9250fbc6bb72d06d4370ff3f77ea926f06c5389c
Jonathan Ellithorpe <jde@cs.stanford.edu>
2018-06-27 04:08:27 +0000

    Added mlnx-seutp.sh

---
+++ mlnx-setup.sh
@@ -0,0 +1,6 @@
+echo -e "\n===== INSTALLING MELLANOX OFED ====="
+OS_VER="ubuntu`lsb_release -r | cut -d":" -f2 | xargs`"
+MLNX_OFED="MLNX_OFED_LINUX-3.4-1.0.0.0-$OS_VER-x86_64"
+axel -n 8 -q http://www.mellanox.com/downloads/ofed/MLNX_OFED-3.4-1.0.0.0/$MLNX_OFED.tgz
+tar xzf $MLNX_OFED.tgz
+./$MLNX_OFED/mlnxofedinstall --force --without-fw-update >> ./$MLNX_OFED/install.log
